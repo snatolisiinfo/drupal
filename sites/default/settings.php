a6ef7e3c9510655c001c550bcd051f221dfbd63e
Simone Natoli <s.natoli@siinfo.eu>
2020-08-07 19:02:20 +0200

    adding hash_salt in settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -298,7 +298,7 @@ $databases['default']['default'] = [
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = '';
+$settings['hash_salt'] = 'sdofwiogoeirgoerngoerngiuergneiurbg';
 
 /**
  * Deployment identifier.
